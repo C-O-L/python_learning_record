1e2ad3b1c92347db91f8278c80ee5f538cc4a7d0
COL <3056990779@qq.com>
2020-05-07 17:53:33 +0800

    second_class

---
+++ second_class.py
@@ -0,0 +1,27 @@
+# second_ce=lass_on['格列佛游记','海底两万里','繁星·春水'];
+# # print (second_class_one);
+# second_class_one.append('伊索寓言');
+# second_class_one.insert(3,'海底两万里');
+# last_owned = second_class_one.pop();
+# print(last_owned);
+# lose_owned = second_class_one.pop(1);
+# second_class_one.remove('繁星·春水');
+# del second_class_one[0];
+# print(second_class_one);
+
+# user_lists = ['张龙','赵虎','王朝','马汉','展昭'];
+# for user_list in user_lists:
+#   print(user_list.title() + "身手了得");
+# print(user_lists[-1] + "是本站SVIP" + "\n")
+
+# number = list(range(0,100,3));
+# min_number = min(number); 
+# max_number = max(number); 
+# sum_number = sum(number);
+# # print(min_number);
+# # print(max_number);
+# # print(sum_number);
+# some_number = number[0:1];
+# # print(some_number);
+# for some_numbers in number [0:10]:
+#   print(some_numbers)
