9ad218a24ab882690371e559e7bf9ec7316daa5c
COL <3056990779@qq.com>
2020-05-14 17:39:47 +0800

    third_class_learning

---
+++ third_class.py
@@ -0,0 +1,49 @@
+# users = ['王朝','马汉','张龙','赵虎','展昭'];
+# for user in users:
+#     if user == '展昭':
+#         print(user.upper() + "你是本朝VIP")
+#     else:
+#         print(user + "身手了得")
+
+# point = ['nd_zwxq','yingli_street','cinema_zw',
+# 'natatorium_zw','hlodayinn_yu','gulou','gaomiao'];
+# print("please input the location:");
+# location = input();
+# if location in point:
+#     print(location + "  distribution")
+# else:
+#     print(location + "  not distribution")
+
+# active = True
+# while active:
+#     nums = int(input ('enter nums:'));
+#     if(nums == -1):
+#         active = False
+#     elif (nums < 2):
+#         print("leaver 1")
+#     elif (nums >= 2 and nums <= 4):
+#         print("leaver 2")
+#     elif (nums > 4 and nums < 11):
+#         print("leaver 3")
+#     elif (nums > 10 and nums < 21):
+#         print("leaver 4")
+#     elif (nums > 20 and nums < 51):
+#         print("leaver 5")
+#     else:
+#         print("leaver 6")
+
+# user_leaver = ['leaver 1','leaver 2','leaver 3',
+# 'leaver 4','leaver 5','leaver 6'];
+# fts = int(input('enter fts:'))
+# if fts<2:
+#     print ("用户等级为："+user_leaver.pop(0))
+# elif fts>2 and fts<4:
+#     print ("用户等级为："+user_leaver.pop(1))
+# elif fts>5 and fts< 10:
+#     print ("用户等级为："+user_leaver.pop(2))
+# elif fts>11 and fts< 20:
+#     print ("用户等级为："+user_leaver.pop(3))
+# elif fts>21 and fts< 50:
+#     print ("用户等级为："+user_leaver.pop(4))
+# else:
+#     print ("用户等级为："+user_leaver.pop(5))
